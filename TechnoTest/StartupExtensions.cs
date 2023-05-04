b1d26a82a8ed8a224b06fc81dcca9e0d44bc87d5
Andrey Iliev <iliev.andreyy@gmail.com>
2023-05-04 23:47:37 +0300

    Added PostgreSQL

---
+++ TechnoTest/StartupExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TechnoTest.Infrastructure;
+
+namespace TechnoTest
+{
+    public static class StartupExtensions
+    {
+        public static void AddCustomPostgreSql(this IServiceCollection serviceCollection,IConfiguration config)
+        {
+            serviceCollection.AddDbContext<IdentityContext>(opt =>
+            {
+                opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+            });
+        }
+    }
+}
