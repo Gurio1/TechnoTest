d386b7a5ea28d1220e02b49cbe0756831d4a51e4
Andrey Iliev <iliev.andreyy@gmail.com>
2023-05-04 23:51:03 +0300

    Added GetAll and Get methods

---
+++ TechnoTest/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechnoTest.Infrastructure;
+using TechnoTest.Models.Identity;
+using TechnoTest.ViewModels;
+
+namespace TechnoTest.Controllers
+{
+    [ApiController]
+    [Route("/api/users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IdentityContext _db;
+
+        public UserController(IdentityContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAll()
+        {
+            var users = await _db.Users.ToListAsync();
+
+            return users;
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get(int id)
+        {
+            var user = await _db.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+        
+        [HttpPost]
+        public async Task Post(UserRegistrationViewModel userVM)
+        {
+        }
+    }
+}
