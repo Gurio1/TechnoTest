4cf630727ab45a9fe71ae0d318f9963c149182ab
Andrey Iliev <iliev.andreyy@gmail.com>
2023-05-04 23:46:52 +0300

    Added DbContext

---
+++ TechnoTest/Infrastructure/IdentityContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using TechnoTest.Models.Identity;
+
+namespace TechnoTest.Infrastructure
+{
+    public class IdentityContext : DbContext
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+        
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserGroup> UserGroups { get; set; }
+        public DbSet<UserState> UserStates { get; set; }
+    }
+}
