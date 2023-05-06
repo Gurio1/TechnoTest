53e6826401e61cccd37a7110dd913ee28abeb42f
Andrey Iliev <iliev.andreyy@gmail.com>
2023-05-06 21:05:34 +0300

    Added migration

---
+++ TechnoTest/Infrastructure/Migrations/20230505203443_Initial.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TechnoTest.Infrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User_Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Code = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    Description = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User_States",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Code = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    Description = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User_States", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Login = table.Column<string>(type: "text", nullable: true),
+                    Password = table.Column<string>(type: "text", nullable: true),
+                    RegistrationDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UserGroupId = table.Column<int>(type: "integer", nullable: false),
+                    UserStateId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_User_Groups_UserGroupId",
+                        column: x => x.UserGroupId,
+                        principalTable: "User_Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Users_User_States_UserStateId",
+                        column: x => x.UserStateId,
+                        principalTable: "User_States",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Login", "Password", "RegistrationDate", "UserGroupId", "UserStateId" },
+                values: new object[] { 1, "Andrey", "123456", new DateTime(2023, 5, 5, 20, 34, 42, 463, DateTimeKind.Utc).AddTicks(5599), 0, 0 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserGroupId",
+                table: "Users",
+                column: "UserGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserStateId",
+                table: "Users",
+                column: "UserStateId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "User_Groups");
+
+            migrationBuilder.DropTable(
+                name: "User_States");
+        }
+    }
+}
