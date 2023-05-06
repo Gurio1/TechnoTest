63c8fe282b9627a28d1f58e4cae2ea8289edace5
Andrey Iliev <iliev.andreyy@gmail.com>
2023-05-06 21:04:09 +0300

    Removed unnecessary props

--- TechnoTest/ViewModels/UserViewModel.cs
+++ TechnoTest/ViewModels/UserViewModel.cs
@@ -8,11 +8,8 @@ namespace TechnoTest.ViewModels
         public int  Id { get; set; }
         public string Login { get; set; }
         public DateTime RegistrationDate { get; set; }
-
-        public int UserGroupId { get; set; }
-        public UserGroup UserGroup { get; set; }
         
-        public int UserStateId { get; set; }
+        public UserGroup UserGroup { get; set; }
         public UserState UserState { get; set; }
     }
 }
