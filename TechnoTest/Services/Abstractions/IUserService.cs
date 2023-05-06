e12e5f36ddd81459436718403767bf31bae57bb3
Andrey Iliev <iliev.andreyy@gmail.com>
2023-05-06 21:01:56 +0300

    Added UserService

---
+++ TechnoTest/Services/Abstractions/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+using TechnoTest.Models.Identity;
+
+namespace TechnoTest.Services.Abstractions;
+
+public interface IUserService
+{
+    Task<User> GetByIdAsync(int id);
+}
---
+++ TechnoTest/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using TechnoTest.Infrastructure.Repositories;
+using TechnoTest.Infrastructure.Repositories.Abstractions;
+using TechnoTest.Models.Identity;
+using TechnoTest.Services.Abstractions;
+using TechnoTest.Specifications.UserSpecifications;
+
+namespace TechnoTest.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
+    public async Task<User> GetByIdAsync(int id)
+    {
+        var specifications = new UserWithGroupSpecification()
+            .And(new UserWithStateSpecification())
+            .And(new UserByIdSpecification(id));
+
+        return  await _userRepository.GetByIdAsync(1, specifications);
+    }
+}
