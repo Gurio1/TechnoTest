282d57c91e517e0a02e2054ab0a71a5412159fa6
Andrey Iliev <iliev.andreyy@gmail.com>
2023-05-06 21:04:49 +0300

    Rename

---
+++ TechnoTest/ProgramExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TechnoTest.Infrastructure;
+public static class StartupExtensions
+{
+    public static void AddCustomPostgreSql(this IServiceCollection serviceCollection, IConfiguration config)
+    {
+        serviceCollection.AddDbContext<IdentityContext>(opt =>
+        {
+            opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+        });
+    }
+}
