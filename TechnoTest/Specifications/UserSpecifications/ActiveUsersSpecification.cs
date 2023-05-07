5774d0af8e9ff9a713f29f44b926abf0f64ace94
Andrey Iliev <iliev.andreyy@gmail.com>
2023-05-07 21:56:44 +0300

    Added new specifications

---
+++ TechnoTest/Specifications/UserSpecifications/ActiveUsersSpecification.cs
@@ -0,0 +1,12 @@
+﻿using TechnoTest.Models.Enums;
+using TechnoTest.Models.Identity;
+
+namespace TechnoTest.Specifications.UserSpecifications;
+
+public class ActiveUsersSpecification : BaseSpecifications<User>
+{
+    public ActiveUsersSpecification()
+    {
+        SetFilterCondition(u =>u.UserState.Code == UserStatus.Active.ToString());
+    }   
+}
---
+++ TechnoTest/Specifications/UserSpecifications/AdminUserSpecification.cs
@@ -0,0 +1,12 @@
+﻿using TechnoTest.Models.Enums;
+using TechnoTest.Models.Identity;
+
+namespace TechnoTest.Specifications.UserSpecifications;
+
+public class AdminUserSpecification : BaseSpecifications<User>
+{
+    public AdminUserSpecification()
+    {
+        SetFilterCondition(u =>u.UserGroup.Code == UserRole.Admin.ToString());
+    }
+}
