b0eb5a6ee170432ea839f611436786950ee70199
Andrey Iliev <iliev.andreyy@gmail.com>
2023-05-07 22:03:58 +0300

    Update controller and GetById method

--- TechnoTest/Controllers/UserController.cs
+++ TechnoTest/Controllers/UserController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using TechnoTest.Infrastructure;
-using TechnoTest.Models.Identity;
+using TechnoTest.Mapping;
+using TechnoTest.Services.Abstractions;
 using TechnoTest.ViewModels;
 
 namespace TechnoTest.Controllers
@@ -13,36 +10,39 @@ namespace TechnoTest.Controllers
     [Route("/api/users")]
     public class UserController : ControllerBase
     {
-        private readonly IdentityContext _db;
+        private readonly IUserService _userService;
 
-        public UserController(IdentityContext db)
+        public UserController(IUserService userService)
         {
-            _db = db ?? throw new ArgumentNullException(nameof(db));
+            _userService = userService;
         }
         
         [HttpGet]
-        public async Task<ActionResult<List<User>>> GetAll()
+        /*public async Task<ActionResult<List<User>>> GetAll()
         {
             var users = await _db.Users.ToListAsync();
 
             return users;
-        }
+        }*/
         
         [HttpGet("{id}")]
-        public async Task<ActionResult<User>> Get(int id)
+        public async Task<ActionResult<UserViewModel>> Get(int id)
         {
-            var user = await _db.Users.FindAsync(id);
+            var user = await _userService.GetAsync(id);
             if (user == null)
             {
                 return NotFound();
             }
 
-            return user;
+            return Ok(user);
         }
         
         [HttpPost]
-        public async Task Post(UserRegistrationViewModel userVM)
+        public async Task<CreatedAtActionResult> Post(UserRegistrationViewModel userVm)
         {
+            var user = await _userService.CreateAsync(userVm.ToUser(),userVm.UserGroupCode);
+            
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, null);
         }
     }
 }
